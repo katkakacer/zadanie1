eb50652b5f9761262bbe355698b0b62e5500a92a
Katarina Kacer <katka@DESKTOP-G60UIO9.localdomain>
2021-11-30 13:34:04 +0100

    hotové zadanie

--- markdown.sh
+++ markdown.sh
@@ -12,7 +12,7 @@ ZOZ=0
 
 while IFS= read LINE
 do
-    if echo "$LINE" | grep '^.*<https://[^>]*>' >/dev/null
+    if echo "$LINE" | grep '<https://[^>]*>' >/dev/null
     then 
         LINE=$(echo "$LINE" | sed 's@<https://\([^>]*\)>@<a href="https://\1">https://\1</a>@g')
         echo "$LINE"
@@ -21,10 +21,9 @@ do
 
     if echo "$LINE" | grep '^ - ' > /dev/null
     then
-
         if [ "$ZOZ" = 0 ]
         then
-            echo '<ol>'
+            echo '<ul>'
             ZOZ=1
         fi
         LINE=$(echo "$LINE" | sed 's@ - \(.*\)@<li>\1</li>@')
@@ -33,32 +32,41 @@ do
     else
         if [ "$ZOZ" = 1 ]
         then
-            echo '</ol>'
+            echo '</ul>'
         fi
     fi
 
     if echo "$LINE" | grep '^[[:space:]]*$' > /dev/null
     then
         echo '<p>'
+        continue
     elif echo "$LINE" | grep '^# ' > /dev/null
     then
         LINE=$(echo "$LINE" | sed 's@# @@')
         echo '<h1>'"$LINE"'</h1>'
+        continue
     elif echo "$LINE" | grep '^## ' > /dev/null
     then
         LINE=$(echo "$LINE" | sed 's@## @@')
         echo '<h2>'"$LINE"'</h2>'
-    elif echo "$LINE" | grep '\(__\)[^_]*\1' > /dev/null
+        continue
+    fi
+
+    if echo "$LINE" | grep '\(__\)[^_]*\1' > /dev/null
     then
         LINE=$(echo "$LINE" | sed 's@__\([^_]*\)__@<strong>\1</strong>@g')
         echo "$LINE"
-    elif echo "$LINE" | grep '\(_\)[^_]*\1' > /dev/null
+        continue
+    fi
+    if echo "$LINE" | grep '\(_\)[^_]*\1' > /dev/null
     then
         LINE=$(echo "$LINE" | sed 's@_\([^_]*\)_@<em>\1</em>@g')
         echo "$LINE"
-    else 
-        echo "$LINE"    
-    fi
+        continue
+    fi    
+ 
+    echo "$LINE"    
+    
 done    
 
 cat << THE_END
